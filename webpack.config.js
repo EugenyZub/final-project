9ba6739c3a6ecba800d6f6e9a1bf4ebb5d7e96f6
Ëugene <evgeniys1@tut.by>
2018-10-30 20:20:03 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  // mode: 'none',
   entry: './src/js/script.js',
   output: {
     filename: 'bundle.js',
@@ -17,28 +16,28 @@ module.exports = {
   devtool: "source-map",
 
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      // include: [],
-      use: {
-        loader: 'babel-loader?optional[]=runtime',
-        options: {
-          presets: [
-            ["@babel/env", {
-              targets: {
-                edge: "17",
-                firefox: "60",
-                chrome: "67",
-                safari: "11.1",
-                ie: "11"
-              },
-              useBuiltIns: "usage"
-            }]
-          ]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader?optional[]=runtime',
+          options: {
+            presets: [
+              ["@babel/env", {
+                targets: {
+                  edge: "17",
+                  firefox: "60",
+                  chrome: "67",
+                  safari: "11.1"
+                },
+                useBuiltIns: "usage"
+              }]
+            ]
+          }
         }
       }
-    }]
+    ]
   },
   plugins: [
     new UglifyJsPlugin()
